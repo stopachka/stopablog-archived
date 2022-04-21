846ccf625c3149c1ad4d05d715bac534a472fc2b
stopachka <stepan.p@gmail.com>
2022-04-21 13:42:46 -0400

    sm

--- src/Header.js
+++ src/Header.js
@@ -13,31 +13,35 @@ function Header({gitHub, adminLinks}) {
           alignItems: 'baseline',
           padding: '20px',
         }}>
-        <h4
-          className="app-header-title"
-          style={{paddingRight: '10px', margin: 0, fontWeight: 500}}>
+        <h4 className="app-header-title" style={{margin: 0, fontWeight: 500}}>
           <Link href={'/'}>
             <a style={{color: 'inherit'}}>
               Stepan <span className="app-header-last-name">Parunashvili</span>
             </a>
           </Link>
           <a
-            style={{paddingLeft: '1em'}}
+            style={{paddingLeft: '10px'}}
             href="https://twitter.com/stopachka"
             target="_blank">
             Twitter
           </a>
           <a
-            style={{paddingLeft: '1em'}}
+            style={{paddingLeft: '10px'}}
+            href="https://instantdb.dev"
+            target="_blank">
+            Instant
+          </a>
+          <a
+            style={{paddingLeft: '10px'}}
             href="https://www.zeneca.io/stopa"
             target="_blank">
             Books
           </a>
           <a
-            style={{paddingLeft: '1em'}}
-            href="https://instantdb.dev"
+            style={{paddingLeft: '10px'}}
+            href="https://www.consistent.fit"
             target="_blank">
-            Instant
+            Fitness
           </a>
         </h4>
       </header>
