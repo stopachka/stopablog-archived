470cf8b6d97c6fab6e063a2141e09842ac90524d
Daniel Woelfel <dwwoelfel@gmail.com>
2023-03-02 14:37:40 -0800

    remove attribution

--- src/Attribution.js
+++ src/Attribution.js
@@ -3,27 +3,5 @@ import {Box, Text, Grommet} from 'grommet';
 import ConfigContext from './ConfigContext';
 
 export default function Attribution() {
-  const {
-    config: {hideAttribution},
-  } = React.useContext(ConfigContext);
-
-  if (hideAttribution) {
-    return null;
-  }
-  return (
-    <Box pad={{bottom: 'small'}} align="center">
-      <Text size="xsmall">
-        Powered by{' '}
-        <a href="https://www.onegraph.com/oneblog" target="_blank">
-          OneBlog
-        </a>{' '}
-        with{' '}
-        <a
-          href="https://docs.netlify.com/netlify-labs/experimental-features/netlify-graph/"
-          target="_blank">
-          Netlify Graph
-        </a>
-      </Text>
-    </Box>
-  );
+  return null;
 }
